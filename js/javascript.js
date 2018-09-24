b7dd0aa9826af0b7fb4bb1df340e48734ab91171
jondood <43475536+jondood@users.noreply.github.com>
2018-09-24 14:57:26 +0100

    Cleaned HTML and JS a tiny bit

--- js/javascript.js
+++ js/javascript.js
@@ -1,31 +1,10 @@
-/*
-$(function(){
-	m = "09/06/2018";
-    d = moment(m).format("YYYY-MM-DD");
-    $("#paragraph").text(d);
-});
-*/
-
-/*
-$(function(){
-	window.onscroll = function() {isPageScrolling()};
-	function isPageScrolling() {
-		if($("#navbarmainpage").visible()) {
-			$("#navbarmainpage").addClass("stickyimportant");
-			else {
-				$("#navbarmainpage").removeClass("stickyimportant");
-			}
-		}
-	}
-});
-*/
-
-var stickyOffset = $(".sticky").offset().top;
+var stickyOffset = $("#navbar").offset().top;
 
 $(window).scroll(function(){
-	var sticky = $(".sticky"),
+	var sticky = $("#navbar"),
 		scroll = $(window).scrollTop();
 
-	if (scroll >= stickyOffset) sticky.addClass("stickyimportant");
-	else sticky.removeClass("stickyimportant");
+	if (scroll >= stickyOffset) sticky.addClass("sticky");
+	else sticky.removeClass("sticky");
 });
+
